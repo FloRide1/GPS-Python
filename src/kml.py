e70fd7fc5a09c77dc014996406e57007e3084a64
FloRide1 <florian.reimat@orange.fr>
2021-03-20 18:29:40 +0100

    Add again a few kml lines + Make more global functions for lines

--- src/kml.py
+++ src/kml.py
@@ -5,6 +5,9 @@ def get_kml_data(coordinates):
     name_line = get_name_line()
     description_line = get_description_line()
     style_line = get_style_line()
+    placemark_placeholder_lines = get_bracket_line("Placemark", 16) 
+    placemark_name_line = get_name_line("Absolute Extruded", 24)
+    placemark_desc_line = get_description_line(" ", 24)
     
     kml_final_line = []
     kml_final_line.append(xml_line)
@@ -13,6 +16,12 @@ def get_kml_data(coordinates):
     kml_final_line.append(name_line)
     kml_final_line.append(description_line)
     kml_final_line.append(style_line)
+    
+
+    kml_final_line.append(placemark_placeholder_lines['begin'])
+    kml_final_line.append(placemark_name_line)
+    kml_final_line.append(placemark_desc_line)
+    kml_final_line.append(placemark_placeholder_lines['end'])
 
     kml_final_line.append(kml_placeholder['end'])
     kml_file = "\n".join(kml_final_line)
@@ -40,43 +49,49 @@ def get_placeholder_kml(xmlns = "http://earth.google.com/kml/2.1",indent_lvl = 8
     return placeholder
 
 def get_name_line(name = "Paths", indent_lvl = 16):
-    line = "{indent}<name>{nme}</name>".format(
-        indent = indent_lvl * " ",
-        nme = name
-    )
+    line = get_unique_line("name",name, indent_lvl)
     return line
 
 def get_description_line(description = "My Journey", indent_lvl = 16):
-    line = "{indent}<description>{dscp}</description>".format(
-        indent = indent_lvl * " ",
-        dscp = description 
-    )
+    line = get_unique_line("description", description, indent_lvl)
     return line
 
 def get_style_line(name_id = "yellowLineGreenPoly", line_color = 2130771967, line_width = 4, 
     poly_color = 2130771967, indent_base = 16, indent_lvl = 8):
     b_Style_line = "{indent}<Style id=\"{n_id}\">".format(indent = indent_base * " ", n_id = name_id)
-    b_Line_line = "{indent}<LineStyle>".format(indent = (indent_base + 1 * indent_lvl) * " ")
-    line_color_line = "{indent}<color>{l_color:8x}</color>".format(indent = (indent_base + 2 * indent_lvl) * " ", l_color = line_color)
-    line_width_line = "{indent}<width>{l_width}</width>".format(indent = (indent_base + 2 * indent_lvl) * " ", l_width = line_width)
-    e_Line_line = "{indent}</LineStyle>".format(indent = (indent_base + 1 * indent_lvl) * " ")
-    poly_color_line = "{indent}<color>{l_color:8x}</color>".format(indent = (indent_base + 2 * indent_lvl) * " ", l_color = poly_color)
-    b_Poly_line = "{indent}<PolyStyle>".format(indent = (indent_base + 1 * indent_lvl) * " ")
-    e_Poly_line = "{indent}</PolyStyle>".format(indent = (indent_base + 1 * indent_lvl) * " ")
     e_Style_line = "{indent}</Style>".format(indent = indent_base * " ")
+
+    Line_line = get_bracket_line("LineStyle", indent_base + indent_lvl)
+    Poly_line = get_bracket_line("PolyStyle", indent_base + indent_lvl)
+
+    line_color_line = get_unique_line("color", "{:8x}".format(line_color),indent_base + 2 * indent_lvl)
+    poly_color_line = get_unique_line("color", "{:8x}".format(poly_color),indent_base + 2 * indent_lvl)
+    line_width_line = get_unique_line("width", line_width,indent_base + 2 * indent_lvl)
 
     global_line = []
     global_line.append(b_Style_line)
-    global_line.append(b_Line_line)
+    global_line.append(Line_line['begin'])
     global_line.append(line_color_line)
     global_line.append(line_width_line)
-    global_line.append(e_Line_line)
-    global_line.append(b_Poly_line)
+    global_line.append(Line_line['end'])
+    global_line.append(Poly_line['begin'])
     global_line.append(poly_color_line)
-    global_line.append(e_Poly_line)
+    global_line.append(Poly_line['end'])
     global_line.append(e_Style_line)
 
     global_line = "\n".join(global_line)
 
     return global_line
 
+def get_unique_line(name, value, indent):
+    line = "{indent_b}<{nme}>{val}</{nme}>".format(indent_b = indent * " ", nme = name, val = value)
+    return line
+
+def get_bracket_line(name, indent):
+    begin = "{indent_b}<{nme}>".format(indent_b=indent * " ", nme=name)
+    end = "{indent_b}</{nme}>".format(indent_b=indent * " ", nme=name)
+    line = {
+        'begin': begin,
+        'end': end
+    }
+    return line
