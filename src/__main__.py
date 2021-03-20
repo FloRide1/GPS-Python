a8d5e7b008ad75e3a56c44e80b1218e1a18c8ca1
FloRide1 <florian.reimat@orange.fr>
2021-03-20 16:39:21 +0100

    Split file for parser and make an GGA parser

--- src/__main__.py
+++ src/__main__.py
@@ -1,3 +1,4 @@
+from parser import *
 
 DATA_PATH = "./data/"
 DATA_FILE = "data_example.txt"
@@ -28,45 +29,6 @@ def main():
         return -1
 
     print("[OK] Main End")
-
-def parse_file(file_data):
-    try:
-        data_list = file_data.split("\n") # Split Each Line of Data
-        print("[OK] File is correctly parsed")
-        correct_data = []
-        for i in range(len(data_list)):
-            data = data_list[i]
-            frame_data = parse_frame(data,i + 1)
-            if frame_data == -1:
-                raise TypeError("Parse failed")
-            correct_data.append(frame_data)
-        return correct_data
-    except:
-        print("[ERROR] Parsing File is impossible")
-        return -1
-    
-
-def parse_frame(data, line):
-    try:
-        parse_data = data.split(",")
-        # Data Form: [type, ]
-        typeOfFrame = parse_data[0][3:] # Take only take the X: $GPXXX
-        if (typeOfFrame == "GGA"):
-            frame_data = {
-                'time'            : int(parse_data[1]),
-                'latitude'        : float(parse_data[2]),
-                'NorthOrSouth'    : parse_data[3],
-                'longitude'       : float(parse_data[4]),
-                'EstOrWest'       : parse_data[5],
-                'quality'         : float(parse_data[6])
-            }
-        else:
-            frame_data = 0
-        return frame_data
-    except:
-        print("[ERROR] Parsing had failed - line: " + str(line))
-        return -1
-
 
 if __name__ == "__main__":
     init()
---
+++ src/parser.py
@@ -0,0 +1,52 @@
+def parse_file(file_data):
+    try:
+        data_list = file_data.split("\n") # Split Each Line of Data
+        print("[OK] File is correctly parsed")
+        correct_data = []
+        for i in range(len(data_list)):
+            data = data_list[i]
+            frame_data = parse_frame(data,i + 1)
+            if frame_data == -1:
+                raise TypeError("Parse failed")
+            correct_data.append(frame_data)
+        return correct_data
+    except:
+        print("[ERROR] Parsing File is impossible")
+        return -1
+    
+
+def parse_frame(data, line):
+    try:
+        parse_data = data.split(",")
+        # Data Form: [type, X, X, ...]
+        typeOfFrame = parse_data[0][3:] # Take only the X: $GPXXX
+        if (typeOfFrame == "GGA"):
+            frame_data = parse_GGA_frame(parse_data)
+        else:
+            frame_data = 0
+        return frame_data
+    except:
+        print("[ERROR] Parsing had failed - line: " + str(line))
+        return -1
+
+def parse_GGA_frame(parse_data):
+    try:
+        typeOfFrame = parse_data[0][3:] 
+        if typeOfFrame == "GGA":
+            frame_data = {
+                'type'          : parse_data[0][3:],
+                'time'          : int(parse_data[1]),
+                'latitude'      : float(parse_data[2]),
+                'north_south'   : parse_data[3],
+                'longitude'     : float(parse_data[4]),
+                'est_west'      : parse_data[5],
+                'quality'       : float(parse_data[6])
+            }
+            return frame_data
+        else:
+            print("[ERROR] This frame is not an GGA frame")
+            return -1
+    except:
+        print("[ERROR] This GGA frame is not correctly formated")
+        return -1
+    
